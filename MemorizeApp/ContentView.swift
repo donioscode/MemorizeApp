6535264ac4d257a21d355b31d6aed5e339a076c6
donioscode <doniyornormuxammedov@gmail.com>
2024-01-06 14:23:33 +0500

    Finished RT1 Assignment 1

--- MemorizeApp/ContentView.swift
+++ MemorizeApp/ContentView.swift
@@ -8,18 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻","☠️","😈","🤡","💀"]
+    let emojis = ["👻","☠️","😈","🤡","💀","👹","👺","😷","🤖","👽","🧚‍♀️","🧟"]
+    
+   @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack{
-            ForEach(emojis.indices,id: \.self ) { index in
-                CardView(content: emojis[index])
+        VStack {
+            ScrollView{
+                cards
             }
-            
+            Spacer()
+            cardsCountAdjusters
+          }
+        
+         .padding()
+    }
+    
+    var cardsCountAdjusters: some View {
+        
+    HStack {
+       cardRemover
+        Spacer()
+        cardAdder
+    }
+    .imageScale(.large)
+    .font(.largeTitle)
+      
+    }
+    
+    func cardCountAdjuster(by offset: Int,symbol: String ) -> some View {
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += offset
+            }
+        
+          },label: {
+            Image(systemName: symbol)
+        })
+        
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count )
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self ) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+         
         }
         .foregroundColor(.orange)
-        .padding()
     }
+    
+    var cardAdder: some View {
+       cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
+    }
+    
+    
+    var  cardRemover: some View{
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
 }
+
+
+    
 
 #Preview {
     ContentView()
@@ -31,18 +84,19 @@ struct CardView : View {
     
     var body: some View{
         ZStack {
-                let base = RoundedRectangle(cornerRadius: 12)
-                if isFaceUp{
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
                     base.fill(.white)
                     base.strokeBorder(lineWidth: 2)
-                      Text(content).font(.largeTitle)
-                } else{
-                    base.fill()
+                    Text(content).font(.largeTitle)
+            
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
     
-            }
-            .onTapGesture {
-                isFaceUp = !isFaceUp
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
