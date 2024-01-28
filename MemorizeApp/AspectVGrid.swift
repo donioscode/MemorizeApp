a6334178fe4478253e5dff57dbe704f4888213af
donioscode <doniyornormuxammedov@gmail.com>
2024-01-28 18:28:58 +0500

    added new AspectVGrid SwiftUi file

---
+++ MemorizeApp/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  MemorizeApp
+//
+//  Created by doniyor normuxammedov on 28/01/24.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
